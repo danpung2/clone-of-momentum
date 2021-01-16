729de913f593c8b88e4c165009a699a2567333e0
danpung2 <75434746+danpung2@users.noreply.github.com>
2021-01-16 10:25:55 +0900

    Add files via upload
    2021-01-15

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const secondes = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${secondes < 10 ? `0${secondes}` : secondes}`;    
+}
+
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
